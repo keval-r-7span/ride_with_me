6c6c623d4ce3c432dc49bf2738ca359243c7f43a
Tushar Bhimani <tushar@7span.com>
2024-03-04 10:38:03 +0530

    update in route and message

--- src/routes/driverRoute.js
+++ src/routes/driverRoute.js
@@ -1,22 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup } = require("../controllers/driver/driverSignUp");
+const signup  = require("../controllers/driver/driverSignUp");
+const login  = require("../controllers/driver/driverLogIn");
+const deleteDriver = require("../controllers/driver/driverDelete");
+const updateAvailability = require("../controllers/driver/driverAvailability");
+const verifyJWT = require('../middleware/authMiddleware');
 
-const { login } = require("../controllers/driver/driverLogIn");
-
-const { deleteDriver } = require("../controllers/driver/driverDelete");
-
-const { updateAvailability } = require("../controllers/driver/driverAvailability");
-
-const { verifyJWT } = require('../middleware/authMiddleware');
 
 router.post("../controllers/driver/driverSignUp.js", signup);
-
 router.post("../controllers/driver/driverLogIn.js", login);
-
 router.put("/:id/availability", verifyJWT, updateAvailability);
-
 router.delete("/:id", verifyJWT, deleteDriver);
 
 module.exports = router;
