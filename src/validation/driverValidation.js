fe96a2a1c97bc58523464d492a5a303eb021ef60
keval Rabadiya <kevalrabadiya27@gmail.com>
2024-03-12 16:01:45 +0530

    update

--- src/validation/driverValidation.js
+++ src/validation/driverValidation.js
@@ -1,23 +1,24 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const driverJoiSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
   phoneNumber: Joi.string().min(10).max(10).required(),
-  vehicleDetails : Joi.string(),
+  availability: Joi.string(),
+  vehicleDetails: Joi.string(),
   password: Joi.string().min(3).required(),
-  role: Joi.string().required(),
+  role: Joi.string().default("driver"),
 });
 
-const updateDriverJoiSchema = Joi.object({
-    name: Joi.string().min(3).max(30),
-    email: Joi.string().email(),
-    phoneNumber: Joi.string().min(10).max(10),
-    vehicleDetails : Joi.string(),
-    availability: Joi.string().valid('available', 'unavailable'),
-    password: Joi.string().min(3),
-    role: Joi.string().valid('driver'),
-}).min(1);
+// const updateDriverJoiSchema = Joi.object({
+//     name: Joi.string().min(3).max(30),
+//     email: Joi.string().email(),
+//     phoneNumber: Joi.string().min(10).max(10),
+//     vehicleDetails : Joi.string(),
+//     availability: Joi.string().valid('available', 'unavailable'),
+//     password: Joi.string().min(3),
+//     role: Joi.string().default("driver")
+// }).min(1);
 
 module.exports = driverJoiSchema;
-module.exports = updateDriverJoiSchema;
+// module.exports = updateDriverJoiSchema;
