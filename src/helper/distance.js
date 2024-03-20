f0c04bbe0181ba8f0e183b94192fef46d0d871f4
keval Rabadiya <kevalrabadiya27@gmail.com>
2024-03-20 20:15:08 +0530

    updated

--- src/helper/distance.js
+++ src/helper/distance.js
@@ -2,7 +2,7 @@ const { DISTANCE } = require("../helper/constants");
 const {trueResponse, falseResponse} = require('../configs/responseMes')
 const dotenv = require("dotenv");
 dotenv.config();
-const logger = require('../utils/logger')
+const logger = require('../utils/indexLogger')
 
 const calcDistance = async (req, res) => {
   try {
@@ -14,8 +14,9 @@ const calcDistance = async (req, res) => {
     const traveledDistance = data.rows[0].elements[0].distance.value;
     const totalFare = Math.ceil(traveledDistance * 0.022);
     logger.info(`Total Fare :${totalFare} INR`);
-  
-    return trueResponse(res, data)
+    
+
+    return trueResponse(res, response)
   } catch (error) {
     return falseResponse(res, error)
   }
